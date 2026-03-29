cc208a67bb8cadde6866229ab473affab0ce3b55
Leon Si <leon@leonsilicon.com>
2026-03-29 15:50:00 -0400

    update

--- src/js/jetbrains.ts
+++ src/js/jetbrains.ts
@@ -3,57 +3,57 @@ import { outdent } from "outdent";
 import path from "path";
 import spawn from "nano-spawn-compat";
 
-const tmp = process.env.TMPDIR ?? "/tmp";
-
 type Context = {
   controller: AbortController
 }
 
-declare global {
-  interface ImportMeta {
-    ideBinPath: string
+export default function buildAction(ideBinPath: string) {
+  if (!ideBinPath) {
+    throw new Error("IDE binpath must be provided")
   }
-}
 
-// This function makes it possible to programmatically execute IntelliJ commands
-export default async function action(this: Context, commandId: string) {
-  const id = Math.random();
-  const scriptPath = path.join(tmp, `jetbrains_action_${id}.groovy`);
-  const resultPath = path.join(tmp, `jetbrains_action_${id}.txt`);
+  const tmp = process.env.TMPDIR ?? "/tmp";
 
-  const script = outdent`
-    import com.intellij.openapi.actionSystem.ActionManager
+  // This function makes it possible to programmatically execute IntelliJ commands
+  return async function action(this: Context, commandId: string) {
+    const id = Math.random();
+    const scriptPath = path.join(tmp, `jetbrains_action_${id}.groovy`);
+    const resultPath = path.join(tmp, `jetbrains_action_${id}.txt`);
 
-    def actionManager = ActionManager.getInstance()
-    def resultFile = new File(${JSON.stringify(resultPath)})
+    const script = outdent`
+      import com.intellij.openapi.actionSystem.ActionManager
 
-    IDE.application.invokeAndWait {
-      try {
-        def action = actionManager.getAction(${JSON.stringify(commandId)})
-        if (action == null) {
+      def actionManager = ActionManager.getInstance()
+      def resultFile = new File(${JSON.stringify(resultPath)})
+
+      IDE.application.invokeAndWait {
+        try {
+          def action = actionManager.getAction(${JSON.stringify(commandId)})
+          if (action == null) {
+            resultFile.text = "0"
+            return
+          }
+
+          def result = actionManager.tryToExecute(action, null, null, null, false)
+          resultFile.text = result.rejected ? "0" : "1"
+        } catch (Throwable ignored) {
           resultFile.text = "0"
-          return
         }
-
-        def result = actionManager.tryToExecute(action, null, null, null, false)
-        resultFile.text = result.rejected ? "0" : "1"
-      } catch (Throwable ignored) {
-        resultFile.text = "0"
       }
-    }
-  `;
+    `;
 
-  fs.writeFileSync(scriptPath, script);
-  const process = spawn(import.meta.ideBinPath, ["ideScript", scriptPath]);
-  const nodeChildProcess = await process.nodeChildProcess
-  this.controller.signal.addEventListener("abort", () => {
-    nodeChildProcess.kill()
-  });
-  await process;
+    fs.writeFileSync(scriptPath, script);
+    const process = spawn(ideBinPath, ["ideScript", scriptPath]);
+    const nodeChildProcess = await process.nodeChildProcess
+    this.controller.signal.addEventListener("abort", () => {
+      nodeChildProcess.kill()
+    });
+    await process;
 
-  const result = fs.readFileSync(resultPath, "utf8");
-  fs.rmSync(scriptPath);
-  fs.rmSync(resultPath);
+    const result = fs.readFileSync(resultPath, "utf8");
+    fs.rmSync(scriptPath);
+    fs.rmSync(resultPath);
 
-  return result == "1";
+    return result == "1";
+  }
 };
